6035d283b9e870140effdfc58cd03c799b0fd3b4
ryo-ri-16 <ujiro1998@gmail.com>
2025-08-28 21:30:04 +0900

    userテスト

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    nickname { 'テスト太郎' }
+    sequence :email do |n|
+      "test#{n}@example.com"
+    end
+    password { '1111111' }
+    password_confirmation { '1111111' }
+  end
+end
