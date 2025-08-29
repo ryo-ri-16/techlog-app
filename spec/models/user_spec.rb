2f801b6ac69c25a86628442b9a289f2440702c05
ryo-ri-16 <ujiro1998@gmail.com>
2025-08-29 21:02:15 +0900

    postモデル

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,8 @@ describe User do
 
   describe '.first' do
     before do
-      create(:user, nickname: nickname, email: email)
+      @user = create(:user, nickname: nickname, email: email) # 修正
+      @post = create(:post, title: 'タイトル', content: '本文', user_id: @user.id) # 修正
     end
 
     subject { described_class.first }
@@ -16,6 +17,13 @@ describe User do
     it '事前に作成した通りのUserを返す' do
       expect(subject.nickname).to eq('テスト太郎')
       expect(subject.email).to eq('test@example.com')
+    end
+
+    it '紐づくPostの情報を取得できる' do
+      expect(subject.posts.size).to eq(1)
+      expect(subject.posts.first.title).to eq('タイトル')
+      expect(subject.posts.first.content).to eq('本文')
+      expect(subject.posts.first.user_id).to eq(@user.id)
     end
   end
 
@@ -35,7 +43,7 @@ describe User do
 
           it 'User オブジェクトは無効である' do
             expect(user.valid?).to be(false)
-            expect(user.errors[:nickname]).to include('is too long (maximum is 20 characters)')
+            expect(user.errors[:nickname]).to include('は20文字以下に設定して下さい。')
           end
         end
       end
@@ -46,7 +54,7 @@ describe User do
 
           it 'User オブジェクトは無効である' do
             expect(user.valid?).to be(false)
-            expect(user.errors[:nickname]).to include("can't be blank")
+            expect(user.errors[:nickname]).to include("が入力されていません。")
           end
         end
       end
