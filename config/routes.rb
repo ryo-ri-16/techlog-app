65db75ede8bee8de3ee6e9101aecc879017f1193
ryo-ri-16 <ujiro1998@gmail.com>
2025-08-29 22:53:18 +0900

    マイページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get "users/show"
   devise_for :users
   root 'home#top'
 
   resources :posts, only: [:index, :new, :create, :show, :destroy]
+  resources :users, only: [:show]
 end
